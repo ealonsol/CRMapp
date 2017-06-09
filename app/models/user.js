048f882152927326eeafaa3c0830893f982fbf03
ealonso <ealonsol@hotmail.com>
2017-06-09 08:43:10 -0500

    Tercer Commit

--- app/models/user.js
+++ app/models/user.js
@@ -1,26 +1,26 @@
- // grab the packages that we need for the user model
-  var mongoose = require('mongoose');
-  var Schema = mongoose.Schema;
-  var bcrypt = require('bcrypt-nodejs');
+// grab the packages that we need for the user model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
   
-  // user schema 
-  var UserSchema   = new Schema({
+  
+// user schema 
+var UserSchema   = new Schema({
   	name: String,
 	username: { type: String, required: true, index: { unique: true }},
     password: { type: String, required: true, select: false }
- });
+});
  
  // hash the password before the user is saved
- UserSchema.pre('save', (next) => {
+UserSchema.pre('save', function(next){
  	var user = this;
  
  	// hash the password only if the password has been changed or user is new
  	if (!user.isModified('password')) return next();
  
  	// generate the hash
- 	bcrypt.hash(user.password, null, null, function(err, hash) {
+ 	bcrypt.hash(user.password, null, null,function(err, hash) {
  		if (err) return next(err);
- 
  		// change the password to the hashed version
  		user.password = hash;
  		next();
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var port = process.env.PORT || 8080;
 
 var User = require('./app/models/user');
 
+mongoose.connect('mongodb://localhost:27017/db_name');
+
 // APP CONFIGURATION ---------------------
 // use body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({extended : true}));
@@ -30,13 +32,8 @@ app.use(morgan('dev'));
 // ROUTES FOR OUR API
 // =============================
 
-// basic route for the home page
-app.get('/', (req, res)=> {
-	res.send('Welcome to the home page!');
-});
-
- // get an instance of the express router
-var apiRouter = express.Router();
+// get an instance of the express router
+var apiRouter = express.Router(); // get an instance of the express Router
 
 // middleware to use for all requests
 apiRouter.use((req, res, next)=> {
@@ -46,22 +43,58 @@ apiRouter.use((req, res, next)=> {
  	// this is where we will authenticate users
  	next(); // make sure we go to the next routes and don't stop here
  });
- 
- // test route to make sure everything is working 
- // (accessed at GET http://localhost:8080/api)
-apiRouter.get('/', (req, res)=> {
+
+// test route to make sure everything is working 
+// (accessed at GET http://localhost:8080/api)
+apiRouter.get('/', function(req, res) {
  	res.json({ message: 'hooray! welcome to our api!' });	
 });
-// test route to make sure everything is working 
-// accessed at GET http://localhost:8080/api
-apiRouter.get('/', (req, res)=> {
-	res.json({ message: 'hooray! welcome to our api!' });	
+
+// basic route for the home page
+app.get('/', (req, res)=> {
+	res.send('Welcome to the home page!');
 });
+
 // more routes for our API will happen here
+// route middleware and first route are here
+// on routes that end in /users
+// ----------------------------------------------------
+apiRouter.route('/users') 
+ 	// create a user (accessed at POST http://localhost:8080/api/users)
+ 	.post((req, res) => {
+ 		// create a new instance of the User model
+ 		var user = new User(); 		
+ 		//set the users information (comes from the request)
+ 		user.name = req.body.name;  
+ 		user.username = req.body.username;
+ 		user.password = req.body.password;
+ 		// save the user and check for errors
+ 		user.save((err) =>{
+             if (err) {
+                 // duplicate entry
+                 if (err.code == 11000) 
+                     return res.json({ success: false, message: 'A user with that username already exists. '});
+                 else 
+                     return res.send(err);
+             }
+ 			res.json({ message: 'User created!' });
+ 		});	
+ 	})
+	// get all the users (accessed at GET http://localhost:8080/api/users)
+	.get(function(req, res) {
+ 		User.find(function(err, users) {
+ 			if (err) res.send(err);
+  			// return the users
+ 			res.json(users);
+ 		});
+ 	});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', apiRouter);
+
 // START THE SERVER
 // ===============================
+
 app.listen(port);
 console.log('Magic happens on port ' + port);
